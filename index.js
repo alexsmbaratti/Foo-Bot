e04220083cfe1a4086e894c0fc6a05148500a859
Alex Baratti <alexsmbaratti@gmail.com>
2022-02-22 00:13:33 -0700

    Adds appropriate role for a bot/user joining the server

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {Client, Intents} = require('discord.js');
-const client = new Client({intents: [Intents.FLAGS.GUILDS]});
+const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS]});
 
 const TOKEN = require('./config.json')['token'];
 const GUILD_ID = require('./config.json')['guildId'];
@@ -8,7 +8,7 @@ var guild;
 client.once('ready', () => {
     console.log(`${client.user.username} is ready!\n`);
     console.log('Invite your bot to your server with this link: ');
-    console.log(`https://discord.com/api/oauth2/authorize?client_id=${client.application.id}&permissions=8&scope=bot%20applications.commands`);
+    console.log(`https://discord.com/api/oauth2/authorize?client_id=${client.application.id}&permissions=8&scope=bot%20applications.commands\n`);
     guild = client.guilds.cache.get(GUILD_ID);
 });
 
@@ -20,7 +20,17 @@ client.on('interactionCreate', async interaction => {
     const {commandName: command} = interaction;
 
     if (command === 'dev') {
-        await toggleRole(interaction, "Developers")
+        await toggleRole(interaction, "Developers");
+    }
+});
+
+client.on('guildMemberAdd', async member => {
+    if (member.user.bot) {
+        console.log("A bot has joined the server.")
+        await member.roles.add(await guild.roles.cache.find(role => role.name === "Bots"), "Auto-assigned with Foo Bot upon join");
+    } else {
+        console.log("A human has joined the server.")
+        await member.roles.add(await guild.roles.cache.find(role => role.name === "Humans"), "Auto-assigned with Foo Bot upon join");
     }
 });
 
